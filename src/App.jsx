522592c3388b0510838797eb62bd70fed60f4e3d
dollyaggarwal123 <dolly.aggarwal@oodles.io>
2024-06-07 18:10:32 +0530

    Completed weather app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import WeatherApp from "./Components/WeatherApp";
+import "./Components/WeatherApp.css";
+function App() {
+  return (
+    <>
+      <WeatherApp />
+    </>
+  );
+}
+
+export default App;
